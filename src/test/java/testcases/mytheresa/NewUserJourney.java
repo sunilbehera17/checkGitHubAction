07a59d0b070108a7b045fc6f0865c22860133920
sunilbehera17 <qaitsunil@gmail.com>
2025-01-06 17:00:35 +0530

    Update NewUserJourney.java

--- src/test/java/testcases/mytheresa/NewUserJourney.java
+++ src/test/java/testcases/mytheresa/NewUserJourney.java
@@ -1,7 +1,8 @@
 package testcases.mytheresa;
 
 import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
